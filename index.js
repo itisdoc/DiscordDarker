67bc644c93f6be4e3298c09449ade7b6b725f6ce
Dune Studios <57120755+dune-studios@users.noreply.github.com>
2022-07-20 00:10:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let code = document.createElement('style'); code.innerText=`span, .userInfoBody-1zgAd0, .clamped-2ZePhX {color: black;}.membersWrap-3NUR2t .hiddenMembers-8kpYM0, .scroller-3X7KbA, .members-3WRCEx, .flex-2S1XBF, .content-2hZxGK .container-3PVapX, .content-FDHp32, .container-3PVapX, .content-FDHp32, .formNotice-2nS8ey, .container-YkUktl, .authedApp-8q3NA9, .ontentColumn-1C7as, .app-3xd6d0 {position: relative;background-color: #090909;}.thin-31rlnD {background-color: relative;}.editor-H2NA06>span, .markup-eYLPri {color: black;}.scrollerContent-2SW0kQ, .container-2sjPya, .markup-eYLPri, .messageContent-2t3eCI, .headerText-1qIDDT, .subtext-2HDqJ7, .customStatus-1UAQAK {color: white;}.member-2gU6Ar {margin-right: 10px;}.form-3gdLxP, .webkit-QgSAqd {margin-top: 10px;}.animatedContainer-2laTjx {z-index: 2;width: 234px;}.container-2o3qEW, .containerDefault-YUSmu3, .containerDefault-3TQ5YN, .sectionDivider-189lqb, .content-2a4AW9, .container-ZMc96U, .title-31SJ6t, .sidebarRegion-1VBisG, .sidebar-nqHbhN, .title-31SJ6t, .container-ZMc96U, .form-3gdLxP, .chatContent-3KubbW, .form-3gdLxP:before, .member-2gU6Ar, .members-3WRCEx {background-color: #202020;}.userInfoBody-1zgAd0, .clamped-2ZePhX, .message-372Ods, em {text-align: center;color: black;}`; document.head.appendChild(code);
+if (document.getElementById('d-darker')) return alert('You already injected D-Darker.');
+let code = document.createElement('style'); code.id = "d-darker"; code.innerText=`span, .userInfoBody-1zgAd0, .clamped-2ZePhX {color: black;}.membersWrap-3NUR2t .hiddenMembers-8kpYM0, .scroller-3X7KbA, .members-3WRCEx, .flex-2S1XBF, .content-2hZxGK .container-3PVapX, .content-FDHp32, .container-3PVapX, .content-FDHp32, .formNotice-2nS8ey, .container-YkUktl, .authedApp-8q3NA9, .ontentColumn-1C7as, .app-3xd6d0 {position: relative;background-color: #090909;}.thin-31rlnD {background-color: relative;}.editor-H2NA06>span, .markup-eYLPri {color: black;}.scrollerContent-2SW0kQ, .container-2sjPya, .markup-eYLPri, .messageContent-2t3eCI, .headerText-1qIDDT, .subtext-2HDqJ7, .customStatus-1UAQAK {color: white;}.member-2gU6Ar {margin-right: 10px;}.form-3gdLxP, .webkit-QgSAqd {margin-top: 10px;}.animatedContainer-2laTjx {z-index: 2;width: 234px;}.container-2o3qEW, .containerDefault-YUSmu3, .containerDefault-3TQ5YN, .sectionDivider-189lqb, .content-2a4AW9, .container-ZMc96U, .title-31SJ6t, .sidebarRegion-1VBisG, .sidebar-nqHbhN, .title-31SJ6t, .container-ZMc96U, .form-3gdLxP, .chatContent-3KubbW, .form-3gdLxP:before, .member-2gU6Ar, .members-3WRCEx {background-color: #202020;}.userInfoBody-1zgAd0, .clamped-2ZePhX, .message-372Ods, em {text-align: center;color: black;}`; document.head.appendChild(code);
 alert('DiscordDarker was installed! Use CTRL+ALT+D to close it!')
 
 document.onkeydown = keydown;
@@ -6,6 +7,7 @@ document.onkeydown = keydown;
 function keydown(evt){
   if (!evt) evt = event;
   if (evt.ctrlKey && evt.altKey && evt.keyCode==68){ //CTRL+ALT+F4
-    alert("CTRL+ALT+F4"); 
+    let delcode = document.getElementById('d-darker')
+    delcode.delete()
   }
 }
